d3ea5720735080bafde9b5184592df170d53bd37
Steve Roth <steve.roth@hpe.com>
2018-08-27 16:40:56 -0700

    Sandbox publish styling for narrow display.

--- assets/sandbox.js
+++ assets/sandbox.js
@@ -7,31 +7,36 @@ window.addEventListener('load', function () {
             var confirmDiv = document.getElementById('publishconfirm');
             var errorDiv = document.getElementById('publisherror');
             submit.setAttribute('disabled', 'disabled');
-            confirmDiv.textContent = '';
-            errorDiv.textContent = '';
+            confirmDiv.style.display = 'none';
+            errorDiv.style.display = 'none';
             fetch('/backend/publish-site', {
                 body: new FormData(publishform),
                 method: 'POST',
             }).then(function (result) {
                 switch (result.status) {
-                case 200:
-                    confirmDiv.textContent = 'Published.';
-                    break;
-                case 400:
-                    errorDiv.textContent = 'The site could not be built.  Check for syntax errors in any changed files.';
-                    break;
-                case 401:
-                    errorDiv.textContent = 'Invalid password.';
-                    break;
-                default:
-                    errorDiv.textContent = 'A processing error occurred.  Please report this error.';
-                    break;
+                    case 200:
+                        confirmDiv.textContent = 'Published.';
+                        confirmDiv.style.display = 'block';
+                        break;
+                    case 400:
+                        errorDiv.textContent = 'The site could not be built.  Check for syntax errors in any changed files.';
+                        errorDiv.style.display = 'block';
+                        break;
+                    case 401:
+                        errorDiv.textContent = 'Invalid password.';
+                        errorDiv.style.display = 'block';
+                        break;
+                    default:
+                        errorDiv.textContent = 'A processing error occurred.  Please report this error.';
+                        errorDiv.style.display = 'block';
+                        break;
                 }
                 if (!result.ok) {
                     submit.removeAttribute('disabled');
                 }
             }).catch(function (err) {
                 errorDiv.textContent = 'A processing error occurred.  Please report this error.';
+                errorDiv.style.display = 'block';
                 submit.removeAttribute('disabled');
             });
         });
